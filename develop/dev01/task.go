a0c560c93829c811bd56d4ddc0dc9594b559533a
evan <evanstukalov@gmail.com>
2024-07-08 15:38:45 +0300

    feat: task_1 completed

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/beevik/ntp"
+	"log"
+	"os"
+)
+
 /*
 === Базовая задача ===
 
@@ -13,5 +19,18 @@ package main
 */
 
 func main() {
+	log.SetOutput(os.Stderr)
 
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		logError(err)
+		os.Exit(1)
+	}
+
+	log.Println("Текущее точное время:", time)
+}
+
+func logError(err error) {
+	log.Printf("Ошибка получения времени: %v", err)
 }
